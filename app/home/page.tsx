910fb1e422f6b610bb1e6b13c98d9a332659568c
Raghavcpp <raghav.cpp@gmail.com>
2025-07-12 11:57:43 +0530

    linkin

---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-100vh flex-col items-center bg-gradient-to-br to-blue-100 from-fuchsia-400'>
+      <h1 className="text-bold animate-bounce">
+        Home is just your dome
+      </h1>
+    </div>
+  )
+}
+
+export default Home
